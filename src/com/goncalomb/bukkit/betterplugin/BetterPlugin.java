d444fdee3bad9ba128fedd28f7c62413cb517553
goncalomb <me@goncalomb.com>
2013-03-14 15:40:17 +0000

    BetterPlugin no longer depends on BukkitReflect

--- src/com/goncalomb/bukkit/betterplugin/BetterPlugin.java
+++ src/com/goncalomb/bukkit/betterplugin/BetterPlugin.java
@@ -1,20 +1,35 @@
 package com.goncalomb.bukkit.betterplugin;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.goncalomb.bukkit.lang.Lang;
-import com.goncalomb.bukkit.reflect.BukkitReflect;
 
 public abstract class BetterPlugin extends JavaPlugin {
-	
+
+	private static SimpleCommandMap _commandMap;
 	private List<BetterCommand> _commands = new ArrayList<BetterCommand>();
-	private SimpleCommandMap _commandMap;
+	
+	protected BetterPlugin() {
+		if (_commandMap == null) {
+			Server server = Bukkit.getServer();
+			Class<?> craftServerClass = server.getClass();
+			try {
+				Method getCommandMap = craftServerClass.getMethod("getCommandMap");
+				_commandMap = (SimpleCommandMap) getCommandMap.invoke(server);
+			} catch (Exception e) {
+				throw new RuntimeException("Error while initializing the BetterPlugin class. This Plugin is not compatible with this version of Bukkit.");
+			}
+		}
+	}
 	
 	public void registerCommand(BetterCommand command) {
 		command._plugin = this;
@@ -24,7 +39,6 @@ public abstract class BetterPlugin extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		_commandMap = BukkitReflect.getCommandMap();
 		Lang.registerPlugin(this);
 	}
 	
