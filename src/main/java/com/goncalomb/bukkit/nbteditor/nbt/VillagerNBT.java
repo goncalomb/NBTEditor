660e72493401720dc5c5f63ccaf7876188a5d4c2
goncalomb <me@goncalomb.com>
2013-12-07 20:52:37 +0000

    Added licence notices to all java files

--- src/main/java/com/goncalomb/bukkit/nbteditor/nbt/VillagerNBT.java
+++ src/main/java/com/goncalomb/bukkit/nbteditor/nbt/VillagerNBT.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2013 - Gonçalo Baltazar <http://goncalomb.com>
+ *
+ * This file is part of NBTEditor.
+ *
+ * NBTEditor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NBTEditor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NBTEditor.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.goncalomb.bukkit.nbteditor.nbt;
 
 import java.util.ArrayList;
