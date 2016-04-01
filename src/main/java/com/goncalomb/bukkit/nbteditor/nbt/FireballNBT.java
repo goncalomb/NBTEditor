866c4cae25c8c4496c8b01fd79313be49413aa52
goncalomb <me@goncalomb.com>
2016-04-01 23:23:59 +0100

    Add 'Direction' variable to fireballs (fixes spawning)

--- src/main/java/com/goncalomb/bukkit/nbteditor/nbt/FireballNBT.java
+++ src/main/java/com/goncalomb/bukkit/nbteditor/nbt/FireballNBT.java
@@ -26,11 +26,13 @@ public class FireballNBT extends EntityNBT {
 
 	static {
 		NBTGenericVariableContainer variables = new NBTGenericVariableContainer("Fireball");
+		variables.add("Direction", new VectorVariable("direction"));
 		variables.add("Power", new VectorVariable("power"));
 		registerVariables(FireballNBT.class, variables);
 	}
 
 	public FireballNBT() {
+		_data.setList("direction", 0.0d, 0.0d, 0.0d);
 		_data.setList("power", 0.0d, 0.0d, 0.0d);
 	}
 
