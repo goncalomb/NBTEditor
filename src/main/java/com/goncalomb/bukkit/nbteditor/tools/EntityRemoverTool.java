4953897b87ac6d6c1445689a31222d350e30cf7c
goncalomb <me@goncalomb.com>
2016-06-13 22:56:39 +0100

    Improve Entity Remover tool to remove all entities near the player

--- src/main/java/com/goncalomb/bukkit/nbteditor/tools/EntityRemoverTool.java
+++ src/main/java/com/goncalomb/bukkit/nbteditor/tools/EntityRemoverTool.java
@@ -20,21 +20,35 @@
 package com.goncalomb.bukkit.nbteditor.tools;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.goncalomb.bukkit.customitems.api.CustomItem;
 import com.goncalomb.bukkit.customitems.api.PlayerDetails;
 
 public final class EntityRemoverTool extends CustomItem {
 
+	private int _radius;
+
 	public EntityRemoverTool() {
 		super("entity-remover", ChatColor.AQUA + "Entity Remover", Material.BLAZE_ROD);
-		setLore(ChatColor.YELLOW + "Right-click an entity to remove it.");
+		setLore(ChatColor.YELLOW + "Right-click an entity to remove it.",
+				ChatColor.YELLOW + "Right-click while sneeking to remove",
+				ChatColor.YELLOW + "all entities in a 3 block radius.");
+		setDefaultConfig("radius", 3);
+	}
+
+	@Override
+	protected void applyConfig(ConfigurationSection section) {
+		super.applyConfig(section);
+		_radius = section.getInt("radius", 3);
 	}
 
 	@Override
@@ -46,6 +60,28 @@ public final class EntityRemoverTool extends CustomItem {
 			event.setCancelled(true);
 		} else {
 			player.sendMessage(ChatColor.RED + "You cannot remove players!");
+		}
+	}
+
+	@Override
+	public void onRightClick(PlayerInteractEvent event, PlayerDetails details) {
+		Player player = event.getPlayer();
+		if (player.isSneaking()) {
+			Location location = event.getPlayer().getLocation();
+			int i = 0;
+			for (Entity entity : location.getWorld().getNearbyEntities(location, _radius, _radius, _radius)) {
+				if (entity.getType() != EntityType.PLAYER) {
+					entity.remove();
+					i++;
+				}
+			}
+			if (i == 1) {
+				player.sendMessage(ChatColor.GREEN + "Removed " + i + " entity.");
+			} else if (i != 0) {
+				player.sendMessage(ChatColor.GREEN + "Removed " + i + " entities.");
+			} else {
+				player.sendMessage(ChatColor.YELLOW + "No entities on a " + _radius + " block radius.");
+			}
 		}
 	}
 
