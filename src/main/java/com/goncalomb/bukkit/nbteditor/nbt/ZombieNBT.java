9715a2994ee7ef51bc70f03875966572f014f3d9
goncalomb <me@goncalomb.com>
2016-08-22 18:49:06 +0100

    Add support for Zombie types (backwards compatible)

--- src/main/java/com/goncalomb/bukkit/nbteditor/nbt/ZombieNBT.java
+++ src/main/java/com/goncalomb/bukkit/nbteditor/nbt/ZombieNBT.java
@@ -27,12 +27,28 @@ public class ZombieNBT extends MobNBT {
 
 	static {
 		NBTGenericVariableContainer variables = new NBTGenericVariableContainer("Zombie");
-		variables.add("IsVillager", new BooleanVariable("IsVillager"));
+		variables.add("Type", new IntegerVariable("ZombieType", 0, 6));
 		variables.add("IsBaby", new BooleanVariable("IsBaby"));
 		variables.add("ConversionTime", new IntegerVariable("ConversionTime", -1));
 		variables.add("CanBreakDoors", new BooleanVariable("CanBreakDoors"));
-		variables.add("VillagerProfession", new IntegerVariable("VillagerProfession", 0, 4));
 		registerVariables(ZombieNBT.class, variables);
+	}
+
+	@Override
+	void onUnserialize() {
+		super.onUnserialize();
+		// Backward compatibility with pre-1.10.
+		if (_data.hasKey("IsVillager")) {
+			if (_data.getByte("IsVillager") != 0) {
+				if (_data.hasKey("VillagerProfession")) {
+					_data.setInt("ZombieType", _data.getInt("VillagerProfession") + 1);
+					_data.remove("VillagerProfession");
+				} else {
+					_data.setInt("ZombieType", 1);
+				}
+			}
+			_data.remove("IsVillager");
+		}
 	}
 
 }
