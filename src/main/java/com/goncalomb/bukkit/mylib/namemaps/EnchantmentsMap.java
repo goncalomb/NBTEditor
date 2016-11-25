277fcc274dd69209a58fb416dbe9520ab384d47f
Gonçalo Baltazar <me@goncalomb.com>
2016-11-25 23:07:00 +0000

    Add new enchantments

--- src/main/java/com/goncalomb/bukkit/mylib/namemaps/EnchantmentsMap.java
+++ src/main/java/com/goncalomb/bukkit/mylib/namemaps/EnchantmentsMap.java
@@ -43,6 +43,7 @@ public final class EnchantmentsMap {
 		_enchantments.put("Thorns", Enchantment.THORNS);
 		_enchantments.put("DepthStrider", Enchantment.DEPTH_STRIDER);
 		_enchantments.put("FrostWalker", Enchantment.FROST_WALKER);
+		_enchantments.put("CurseOfBinding", Enchantment.BINDING_CURSE);
 		_enchantments.put("Sharpness", Enchantment.DAMAGE_ALL);
 		_enchantments.put("Smite", Enchantment.DAMAGE_UNDEAD);
 		_enchantments.put("BaneOfArthropods", Enchantment.DAMAGE_ARTHROPODS);
@@ -60,6 +61,7 @@ public final class EnchantmentsMap {
 		_enchantments.put("LuckOfTheSea", Enchantment.LUCK);
 		_enchantments.put("Lure", Enchantment.LURE);
 		_enchantments.put("Mending", Enchantment.MENDING);
+		_enchantments.put("CurseOfVanishing", Enchantment.VANISHING_CURSE);
 
 		List<String> enchantmentNames = new ArrayList<String>(_enchantments.names());
 		Collections.sort(enchantmentNames, String.CASE_INSENSITIVE_ORDER);
